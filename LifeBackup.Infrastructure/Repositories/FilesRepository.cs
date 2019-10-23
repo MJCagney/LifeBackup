2d5235b21c6d9ac6ed8f2dcde75353a2c28dc9a2
Jim Squires <squires.jim@gmail.com>
2019-10-22 21:34:58 -0500

    initial deploy

---
+++ LifeBackup.Infrastructure/Repositories/FilesRepository.cs
@@ -0,0 +1,11 @@
+﻿using LifeBackup.Core.Communication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LifeBackup.Infrastructure.Repositories
+{
+    public class FilesRepository : IFilesRepository
+    {
+    }
+}
